5c7e6c733e4362fccee67a556b2a3202aabee15b
mmitry <monica_mitry@yahoo.com>
2025-01-28 22:08:05 -0500

    app submission

--- submission/app.py
+++ submission/app.py
@@ -1,12 +1,20 @@
 # Import the dependencies.
+import pandas as pd
+from flask import Flask, jsonify
 
-
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect, text
 
 #################################################
 # Database Setup
 #################################################
 
 # Create engine using the `hawaii.sqlite` database file
+def __init__(self):
+    self.engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+    self.Passenger = self.createPassenger()
 
 # Declare a Base using `automap_base()`
 
